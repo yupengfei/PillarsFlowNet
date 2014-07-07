b9f1b30cf9eb305fcb588d5a2172fd90b4819f3c
yupengfei <yupengfei@foxmail.com>
2014-07-07 16:42:51 +0800

    add log package

--- encoding/TestBase64.go
+++ encoding/TestBase64.go
@@ -9,11 +9,16 @@ func TestBase64Bytes(t * testing.T) {
 	hello := "hello world"
 	debyte := Base64Encode([]byte(hello))
 
-	enbyte := Base64Decode(debyte)
-
+	enbyte, err := Base64Decode(debyte)
+	if err != nil {
+		panic(err.Error())
+	}
+	if hello != string(enbyte) {
+		t.Error("hello is not equal to enbyte")
+	}
 }
 
 // func TestBase64Decode(t * testing.T) {
-	
+
 
 // }
